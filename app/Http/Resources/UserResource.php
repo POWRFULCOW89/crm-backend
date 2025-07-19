117c74a6bbe643e9722696e9b65ac0a0a6697be8
Alexis <alexis@enviosperros.com>
2025-07-18 20:10:20 -0600

    changes

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -10,14 +10,10 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
             'email' => $this->email,
             'name' => $this->name,
             'avatar' => $this->avatar,
-            'role' => 'Admin',
-            'status' => 'Active',
             'created_at' => $this->created_at,
-            'last_login_at' => now(),
         ];
     }
 }
